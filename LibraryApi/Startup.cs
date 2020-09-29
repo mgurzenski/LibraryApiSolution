2bb5a0cc73fd1091c7e3a0c56e763bc4ac0bb284
Jeffry Gonzalez <jeff@hypertheory.com>
2020-09-29 14:35:12 -0400

    done with creating database

--- LibraryApi/Startup.cs
+++ LibraryApi/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryApi.Domain;
 using LibraryApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +31,11 @@ namespace LibraryApi
             services.AddTransient<ISystemTime, SystemTime>(); // create a brand new instance any time it's needed.
             // services.AddScoped<ISystemTime, SystemTime>(); // create exactly one of these PER REQUEST.
             // services.AddSingleton<ISystemTime, SystemTime>(); // Create exactly one of these and share it like a cheap bottle of wine with anybody that needs it.
+
+            services.AddDbContext<LibraryDataContext>(options =>
+            {
+                options.UseSqlServer(/*TODO BAD*/ @"server=.\sqlexpress;database=books_dev;integrated security=true");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
