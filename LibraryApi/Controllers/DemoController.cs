0077a3480f03c0670927ee5f4e822ebad21886f6
Jeffry Gonzalez <jeff@hypertheory.com>
2020-09-29 12:26:13 -0400

    before lunch

--- LibraryApi/Controllers/DemoController.cs
+++ LibraryApi/Controllers/DemoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +12,18 @@ namespace LibraryApi.Controllers
 
         // Route Params
         // GET /employees/19
-        [HttpGet("/employees/{employeeId:int}")]
+        [HttpGet("/employees/{employeeId:int}", Name ="employees#get")]
         public ActionResult LookupEmployee([FromRoute] int employeeId)
         {
-            return Ok($"Finding employee { employeeId }");
+            // go to the database whatever..
+            var response = new GetEmployeeDetailsResponse
+            {
+                Id = employeeId,
+                Name = "Joe Schmidt",
+                Department = "DEV",
+                StartingSalary = 50000
+            };
+            return Ok(response);
         }
 
         // GET /blogs/2020/09/18
@@ -45,7 +54,25 @@ namespace LibraryApi.Controllers
         {
             // Your template for doing a post to a collection.
             // Validate the data. If it is bad, send a 400 with or without details.
-            return Ok($"You want to hire {employeeToHire.Name} in {employeeToHire.Department} at {employeeToHire.StartingSalary:c}");
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            // POST is not idempotent and it is not safe.
+            // - You post to make things happen.
+            //  - do your programmery stuff. Whatever that.
+            // Reponse
+            // 201 Created Status code. 
+            // Add a Location to the response with the URI of the brand new resource.
+            // To send them a copy of that resource - exactly as they'd get from following the location header.
+            var response = new GetEmployeeDetailsResponse
+            {
+                Id = new Random().Next(50, 10000),
+                Name = employeeToHire.Name,
+                Department = employeeToHire.Department,
+                StartingSalary = employeeToHire.StartingSalary
+            };
+            return CreatedAtRoute("employees#get", new { employeeId = response.Id }, response);
 
         }
 
@@ -54,8 +81,23 @@ namespace LibraryApi.Controllers
 
     public class PostEmployeeCreate
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Department { get; set; }
+        [Range(10000,200000)]
+        public decimal StartingSalary { get; set; }
+    }
+
+    public class GetEmployeeDetailsResponse
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Range(10000, 200000)]
         public decimal StartingSalary { get; set; }
     }
 }
